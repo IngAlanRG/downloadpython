d7e2828d14043a391b9fdd3e5cdf1db12ab249bc
IngAlanRG <ingalanrosasgarcia@gmail.com>
2023-05-22 22:47:59 -0600

    agregando archivo.py

---
+++ Download.py
@@ -0,0 +1,28 @@
+from pytube import YouTube
+
+def descargar_video(url):
+    try:
+        # Crea una instancia de la clase YouTube con la URL del video
+        video = YouTube(url)
+        
+        # Filtra los formatos disponibles y selecciona el formato MP4 con mayor resolución
+        formato = video.streams.filter(file_extension='mp4').get_highest_resolution()
+        title = video.title
+        name = video.author
+        # Descarga el video en el formato seleccionado
+        formato.download()
+        
+        print("¡Descarga completada!")
+        print(name ,title)
+    except Exception as e:
+        print("Error al descargar el video:", str(e))
+
+# Aquí coloca la url del video de YouTube
+url = "https://www.youtube.com/watch?v=gwmWEr67hzQ&list=RDQDSuw9IyBwA&index=21"
+descargar_video(url)
+
+#debes instalar python 
+#debes de instalar > pip install pytube
+#Para ejecútalo python Download.py
+#con tabulador autocompleted el nombre 
+#tabulador es la tecla de las dos flechas en inversa a la otra
